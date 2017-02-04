c50185c01c621c71ee04430297851f98d4060c51
aj3x <ajrussell94@hotmail.com>
2017-02-04 16:45:08 -0600

    Astar incomplete

--- AStar.py
+++ AStar.py
@@ -4,20 +4,20 @@ from Position import Pos
 import math
 
 
-def hcost(start, dest):
+def get_hcost(start, dest):
     """
 
     :type start: Pos
     :type dest: Pos
     :param start:
     :param dest:
-    :return:
+    :return: heuristic cost from start to dest
     """
     return math.sqrt(math.pow((start.x-dest.x), 2) + math.pow((start.y-dest.y), 2))
 
 
 class AStarNode:
-    def __init__(self, vertex, prev=None, gcost=0):
+    def __init__(self, vertex, prev=None, gcost=0, hcost=0):
         """
 
         :type vertex: Vertex
@@ -25,20 +25,36 @@ class AStarNode:
         :param vertex: Current vertex data
         :param prev: previous AStarNode
         """
-        self.node = vertex
+        self.data = vertex
         self.prev = prev
-        if prev is not None:
-            self.gCost= prev.gCost + gcost
+        self.hcost= hcost
+        if prev is None:
+            self.gCost = gcost
         else:
-            self.gCost=gcost
+            self.gCost = prev.gCost + gcost
 
 
 def AStar(start, dest):
     """
 
-    :param start:
-    :param dest:
-    :return:
+    :type start: Vertex
+    :type dest: Vertex
+    :param start: start vertex
+    :param dest: destination vertex
+    :return: AStarNode with path to dest from start reversed
     """
-    path = start
+    node_list = None
+    cur_node = AStarNode(start)
+
+
+    while cur_node.data is not dest:
+
+        assert isinstance(cur_node.data, Pos)
+        cur_node.data.x
+
+
+    return cur_node
+
+
+
 
--- Main.py
+++ Main.py
@@ -1,12 +1,86 @@
-from Truck import *
-from Graph import *
+from Position import Pos
+from Package import Package
+from Truck import Truck
+from Graph import Edge
+from Graph import Vertex
+from Graph import Graph
+
+def getVertexByPos(g,x,y):
+    """
+
+    :param g:
+    :type g: Graph
+    :param x:
+    :param y:
+    :return:
+    :rtype:
+    """
+    return g.getVertexByData(Pos(x, y))
 
 
 def TEST():
     print("Test Start")
-    print()
-    p = Pos(1,2)
+    print("Position Tests:", end=" ")
+    p = Pos(1, 2)
+    assert p.x == 1
+    assert p.y == 2
     assert p == p
+    assert str(p) == "(1,2)"
+    print("Passed")
 
-    v = Vertex(p)
-    v.__repr__()
+    print("Package Tests:", end=" ")
+    pack = Package(p)
+    assert pack.x == p.x
+    assert pack.y == p.y
+    assert pack == pack
+    assert str(pack) == "(1,2)"
+    print("Passed")
+
+    print("Truck Tests:", end=" ")
+    t = Truck(p)
+    assert t.x == p.x
+    assert t.y == p.y
+    assert t == t
+    assert str(t) == "(None(1,2))"
+    print("Passed")
+
+    print("Trucks & Packages:", end=" ")
+    assert t.x == pack.x
+    assert t.y == pack.y
+    assert t.__cmp__(pack)
+#   PICK UP PACKAGE
+    t.pickup(pack)
+    assert t.packs is not None
+    assert t.packs == pack
+#   MOVE POSITION
+    t.goto(Pos(2,1))
+    assert t.__cmp__(pack)
+    print("Passed")
+
+    print("Vertex Tests:", end=" ")
+    v = Vertex(Pos(0, 0))
+    assert v.data.__cmp__(Pos(0, 0))
+    assert v.edges == []
+    print("Passed")
+
+    print("Edge Tests:", end=" ")
+#   SIMPLE EDGE TEST
+    e = Edge(v)
+    assert e.weight == 0
+    assert e.nextVertex == v
+#   ADD EDGE POINT
+    v2 = Vertex(Pos(1, 0))
+    v.add_edge(v2)
+    assert v.edges[0].weight == 0
+    assert v.edges[0].nextVertex == v2
+    print("Passed")
+
+    print("Graph Tests:", end=" ")
+    g = Graph()
+    for i in range(0,1):
+        for j in range(0,1):
+            g.add_vertex(Pos(i, j))
+
+    print(getVertexByPos(g, 0, 0))
+
+    print("Passed")
